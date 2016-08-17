b62267b04e1797d8ff7ba257d3b6b9df507e491d
Erik Lövbom <erik.lovbom@studentconsulting.com>
2016-08-17 10:43:47 +0200

    A new solution is uploaded

---
+++ DotNetSummer2016/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="DotNetSummer2016.MvcApplication" Language="C#" %>
---
+++ DotNetSummer2016/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(DotNetSummer2016.Startup))]
+namespace DotNetSummer2016
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
